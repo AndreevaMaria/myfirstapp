93a8e897660db794b03a30154e0985fd2495cdf9
Mary <you@example.com>
2020-03-24 21:01:13 +0700

    добавили header

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyHeaderComponent } from '.c:/Users/Student/Desktop/Andr/angular/1 урок/my-app/src/my-header/my-header.component';
+import { MyHeaderComponent } from './my-header/my-header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      MyHeaderComponent,
+      MyHeaderComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
